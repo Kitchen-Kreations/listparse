f774574edab588bd7dd58491710c03d0327577b4
BlessedToastr <59714988+BlessedToastr@users.noreply.github.com>
2023-02-01 13:12:21 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
-wget "https://github.com/Kitchen-Kreations/listparse/releases/download/v1.0.0/listparse" -O /usr/sbin
